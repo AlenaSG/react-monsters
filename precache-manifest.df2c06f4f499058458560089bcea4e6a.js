d68284d60aa66a1d9df408a22a20d305628dea97
Alena Golovina <alenasgolovina@gmail.com>
2020-04-19 19:30:13 -0700

    Updates

---
+++ precache-manifest.df2c06f4f499058458560089bcea4e6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc637cfe0cbd88a6407f494f259a6a11",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "f853f74ec3b2549ff4ee",
+    "url": "/react-monsters/static/css/main.f651bf6c.chunk.css"
+  },
+  {
+    "revision": "d5ca41ba07a51fbfe6eb",
+    "url": "/react-monsters/static/js/2.e0431636.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.e0431636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f853f74ec3b2549ff4ee",
+    "url": "/react-monsters/static/js/main.41a3d265.chunk.js"
+  },
+  {
+    "revision": "be0371f190bad51c0842",
+    "url": "/react-monsters/static/js/runtime-main.99d72536.js"
+  }
+]);
